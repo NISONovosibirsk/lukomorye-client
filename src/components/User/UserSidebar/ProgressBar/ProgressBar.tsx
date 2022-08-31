7e2a95c1339824223a99fe5ed4128b7d7af38b7b
Alexey Hohlov <alexey.hohl0v@yandex.ru>
2022-08-31 14:10:13 +0700

    feat: progressbar added

--- src/components/User/UserSidebar/ProgressBar/ProgressBar.tsx
+++ src/components/User/UserSidebar/ProgressBar/ProgressBar.tsx
@@ -4,13 +4,24 @@ interface Props {
     value: number;
 }
 
-const ProgressBar: React.FC<Props> = ({value}) => {
+const ProgressBar: React.FC<Props> = ({ value }) => {
+    const handlePercentage = (value: number) => {
+        let deg = (360 / 100) * value;
+        return deg;
+    };
 
     return (
-        <div className={styles.progress}>
+        <div
+            className={styles.progress}
+            style={{
+                background: `conic-gradient(#ED771C ${handlePercentage(
+                    value
+                )}deg, #F2F2F2 0deg)`,
+            }}
+        >
             <span>{`${value}%`}</span>
         </div>
-    )
-}
+    );
+};
 
 export default ProgressBar;
