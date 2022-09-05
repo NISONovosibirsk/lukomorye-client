110d907b74851f7201b5ab8a289277dffa474f20
Alexey Hohlov <alexey.hohl0v@yandex.ru>
2022-09-05 20:41:22 +0700

    feat: random colors for card headers

--- src/components/User/QuizCard/QuizCard.tsx
+++ src/components/User/QuizCard/QuizCard.tsx
@@ -7,14 +7,33 @@ interface Props {
 }
 
 const QuizCard: React.FC<Props> = ({ quiz }) => {
+    const colors: Array<string> = [
+        '#43B05C',
+        '#0D85C9',
+        '#ED771C',
+        '#E2B803',
+        '#5943B0',
+        '#D92020',
+        '#25316D',
+        '#E94560',
+        '#874C62',
+        '#B9005B',
+        '#A66CFF',
+    ];
+
+    const handleColors: () => string = () => {
+        let randomValue = Math.floor(Math.random() * colors.length);
+        return colors[randomValue];
+    };
+
     return (
         <li className={styles.card}>
             <div className={styles.header}>
-                <p>{quiz.name}</p>
+                <p style={{ color: `${handleColors()}` }}>{quiz.name}</p>
                 <ColorsIcon />
             </div>
             <p>{quiz.description}</p>
-            <Button title='Старт' width={'250px'}/>
+            <Button title='Старт' width={'250px'} />
         </li>
     );
 };
