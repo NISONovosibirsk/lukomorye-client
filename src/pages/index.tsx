9a89b89c2e4016410f4843bcbba66776c208297a
Alexey Hohlov <alexey.hohl0v@yandex.ru>
2022-10-11 15:08:39 +0700

    ref: start page fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 const Index = () => {
     return (
-        <div className='app'>
+        <div
+            className='app'
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: '3vh',
+                marginLeft: '3vw',
+                marginTop: '3vh',
+                
+            }}
+        >
             <Link href={'/user'}>Пользователь</Link>
-            <Link href={'/quiz'}>Викторина</Link>
+            {/* <Link href={'/quiz'}>Викторина</Link> */}
+            <Link href={'/sign_in'}>Вход</Link>
         </div>
     );
 };
