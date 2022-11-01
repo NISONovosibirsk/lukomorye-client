0a52ba37771d01058eda8d54a0df08c1151512e8
Alexey Hohlov <alexey.hohl0v@yandex.ru>
2022-11-01 12:47:26 +0700

    ref: footer fixed

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,15 +1,38 @@
-import { ReactNode } from 'react';
+import Link from 'next/link';
+import { FooterLogo, MailIcon, PhoneIcon, PlanetIcon } from '../../../assets';
 import styles from './Footer.module.scss';
 
-interface Props {
-    children?: ReactNode
-}
-
-const Footer: React.FC<Props> = ({children}) => {
+const Footer: React.FC = () => {
 
     return (
         <div className={styles.footer}>
-            {children}
+                    <FooterLogo />
+                    <ul className={styles.links}>
+                        <Link href={'/'}>Главная</Link>
+                        <Link href={'/'}>
+                            Игра-конкурс «Золотой ключик» для 1-4 классов
+                        </Link>
+                        <Link href={'/'}>
+                            Игра-конкурс «Лестница успеха» для 5-11 классов
+                        </Link>
+                        <Link href={'/'}>
+                            Игра-конкурс «Лукоморье» для 1-11 классов
+                        </Link>
+                    </ul>
+                    <ul className={styles.contacts}>
+                        <li>
+                            <PhoneIcon />
+                            <p>(383) 308 35 16</p>
+                        </li>
+                        <li>
+                            <MailIcon />
+                            <p>mail@info.ru</p>
+                        </li>
+                        <li>
+                            <PlanetIcon />
+                            <p>luka@nios.ru</p>
+                        </li>
+                    </ul>
         </div>
     )
 }
